4e095228f0cba84693bab57842ac22d1495d78b2
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-04-06 17:45:07 +0600

    added js event lisner

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,14 @@
 const accordtion_title = document.querySelectorAll(".accordion-title");
 
-// accordtion_title.forEach((title) => {
-//   title.addEventListener("click", (e) => {
-//     title.nextElementSibling.classList.toggle("inactive");
-//     if (!title.nextElementSibling.classList.contains("inactive")) {
-//       title.querySelector(".accordtion_btn").innerHTML = "-";
-//       title.parentElement.style.backgroundColor = "#b9ff66";
-//     } else {
-//       title.querySelector(".accordtion_btn").innerHTML = "+";
-//       title.parentElement.style.backgroundColor = "#f3f3f3";
-//     }
-//   });
-// });
+accordtion_title.forEach((title) => {
+  title.addEventListener("click", (e) => {
+    title.nextElementSibling.classList.toggle("inactive");
+    if (!title.nextElementSibling.classList.contains("inactive")) {
+      title.querySelector(".accordtion_btn").innerHTML = "-";
+      title.parentElement.style.backgroundColor = "#b9ff66";
+    } else {
+      title.querySelector(".accordtion_btn").innerHTML = "+";
+      title.parentElement.style.backgroundColor = "#f3f3f3";
+    }
+  });
+});
