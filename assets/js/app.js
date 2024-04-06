8d4d53bc164b0cd8865dd9ef16102c1ef1261995
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-04-07 02:42:01 +0600

    tablet mode added

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,14 +5,29 @@ accordtion_title.forEach((title) => {
     title.nextElementSibling.classList.toggle("inactive");
     if (!title.nextElementSibling.classList.contains("inactive")) {
       title.querySelector(".accordtion_btn").innerHTML = "-";
-      title.parentElement.style.backgroundColor = "#b9ff66";
+      // title.parentElement.style.backgroundColor = "#b9ff66";
+      title.parentElement.classList.toggle("apply-bg-green");
     } else {
       title.querySelector(".accordtion_btn").innerHTML = "+";
-      title.parentElement.style.backgroundColor = "#f3f3f3";
+      // title.parentElement.style.backgroundColor = "#f3f3f3";
+      title.parentElement.classList.toggle("apply-bg-green");
     }
   });
+
+  // mouseover bg change
+  // if (title.nextElementSibling.classList.contains("inactive")) {
+  //   title.addEventListener("mouseover", (e) => {
+  //     title.parentElement.style.backgroundColor = "#b9ff66";
+  //   });
+  //   title.addEventListener("mouseout", () => {
+  //     title.parentElement.style.backgroundColor = "#f3f3f3";
+  //   });
+  // } else {
+  //   title.parentElement.style.backgroundColor = "#b9ff66";
+  // }
   if (!title.nextElementSibling.classList.contains("inactive")) {
     title.querySelector(".accordtion_btn").innerHTML = "-";
-    title.parentElement.style.backgroundColor = "#b9ff66";
+    // title.parentElement.style.backgroundColor = "#b9ff66";
+    // title.parentElement.classList.toggle("apply-bg-green");
   }
 });
