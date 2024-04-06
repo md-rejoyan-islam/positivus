072f45c5988dba2df6996705ee8dfabb5cb6003d
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-04-06 17:44:40 +0600

    added js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,14 @@
 const accordtion_title = document.querySelectorAll(".accordion-title");
-console.log(accordtion_title);
 
-// console.log(accordtion_btn);
-
-accordtion_title.forEach((title) => {
-  title.addEventListener("click", (e) => {
-    title.nextElementSibling.classList.toggle("inactive");
-    if (!title.nextElementSibling.classList.contains("inactive")) {
-      title.querySelector(".accordtion_btn").innerHTML = "-";
-      title.parentElement.style.backgroundColor = "#b9ff66";
-    } else {
-      title.querySelector(".accordtion_btn").innerHTML = "+";
-      title.parentElement.style.backgroundColor = "#f3f3f3";
-    }
-    // if (
-    //   e.target.classList.contains("accordion-title") ||
-    //   e.target.parentElement.parentElement.classList.contains("accordion-title")
-    // ) {
-    // }
-    //  if()
-  });
-});
+// accordtion_title.forEach((title) => {
+//   title.addEventListener("click", (e) => {
+//     title.nextElementSibling.classList.toggle("inactive");
+//     if (!title.nextElementSibling.classList.contains("inactive")) {
+//       title.querySelector(".accordtion_btn").innerHTML = "-";
+//       title.parentElement.style.backgroundColor = "#b9ff66";
+//     } else {
+//       title.querySelector(".accordtion_btn").innerHTML = "+";
+//       title.parentElement.style.backgroundColor = "#f3f3f3";
+//     }
+//   });
+// });
